8da11f822ec4208621f1e619f2ea19cc47618eab
Jacob Krall <jacob@fogcreek.com>
2013-10-19 18:25:15 -0400

    Proof-of-concept: I can write a proportional controller

--- public/js/main.js
+++ public/js/main.js
@@ -11,8 +11,18 @@ window.onload = function() {
         new Vector(0, 0) // orientation
     );
     addTractor(tractor, 'fill:blue;stroke:rgb(0,0,200);stroke-width:2;');
-    tractor.accelerator.setValue(1.0);
     scene = new BodyFollowScene(tractor.body);
 
+    setInterval(function() {
+        if(tractor.body.center.x > 500 + tractor.body.size.x/2) {
+            // coast to a stop
+            tractor.accelerator.setValue(0);
+        } else {
+            // proportional control
+            var tV = 1.0;
+            var error = (tV - tractor.body.velocity.x) / tV;
+            tractor.accelerator.setValue(error*10);
+        }
+    }, 20);
     setInterval(tick, 20);
 }
